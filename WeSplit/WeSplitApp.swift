64d67839fcef66103443b8adb3a6cf409b244c43
Fernando Belotto <fernando.bbosco@gmail.com>
2021-06-04 08:27:19 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Fernando Gabriel Belotto Bosco on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
